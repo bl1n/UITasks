0a5894e25b4bd35c0195a8d197a5670695392133
sukha <sukhanov87@gmail.com>
2020-01-28 09:37:27 +0300

    bubbles

--- app/src/main/java/team/lf/uitasks/bubbles/BubbleView.kt
+++ app/src/main/java/team/lf/uitasks/bubbles/BubbleView.kt
@@ -1,32 +1,23 @@
 package team.lf.uitasks.bubbles
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
+import android.os.Build
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
 private const val BACKGROUND_COLOR = Color.WHITE
-private const val BUBBLE_COLOR = Color.GRAY
-private const val INNER_BUBBLE_COLOR = Color.GRAY
+private const val BUBBLE_COLOR = Color.BLACK
+private const val INNER_BUBBLE_COLOR = Color.WHITE
 
 class BubbleView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr){
+) : View(context, attrs, defStyleAttr) {
 
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
-
-    private val innerBubblePaint = Paint().apply {
-        color = INNER_BUBBLE_COLOR
-        isAntiAlias = true
-        isDither = true
-        style = Paint.Style.FILL
-    }
 
     private val initBubblePaint = Paint().apply {
         color = BUBBLE_COLOR
@@ -34,6 +25,13 @@ class BubbleView @JvmOverloads constructor(
         isDither = true
         style = Paint.Style.FILL
     }
+    private val innerBubblePAint = Paint().apply {
+        color = INNER_BUBBLE_COLOR
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.FILL
+    }
+
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         if (::extraBitmap.isInitialized) extraBitmap.recycle()
@@ -46,7 +44,12 @@ class BubbleView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
-        canvas.drawCircle((width/2).toFloat(), (width/2).toFloat(), (width/2).toFloat(), initBubblePaint)
+        canvas.drawCircle(
+            (width / 2).toFloat(),
+            (width / 2).toFloat(),
+            (width / 2).toFloat(),
+            initBubblePaint
+        )
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -54,20 +57,23 @@ class BubbleView @JvmOverloads constructor(
             MotionEvent.ACTION_DOWN -> touchDown()
             MotionEvent.ACTION_UP -> touchUp()
         }
-       return true
+        return false
     }
 
     private fun touchDown() {
-        extraCanvas.drawColor(BACKGROUND_COLOR)
-        Log.d("TAG", "down ${(width/2).toFloat()}")
-        extraCanvas.drawCircle((width/2).toFloat(), (width/2).toFloat(), (width/2).toFloat(), innerBubblePaint)
+        Log.d("TAG", "down ${(width / 2).toFloat()}")
+//        extraCanvas.drawCircle(
+//            (width / 2).toFloat(),
+//            (width / 2).toFloat(),
+//            (width / 2).toFloat(),
+//            innerBubblePAint
+//        )
         invalidate()
     }
 
     private fun touchUp() {
-        extraCanvas.drawColor(BACKGROUND_COLOR)
-        Log.d("TAG", "up")
-        extraCanvas.drawCircle((width/2).toFloat(), (width/2).toFloat(), (width/2).toFloat(), initBubblePaint)
+        extraCanvas.drawColor(Color.BLUE)
+
         invalidate()
     }
 }
--- app/src/main/java/team/lf/uitasks/bubbles/BubblesFragment.kt
+++ app/src/main/java/team/lf/uitasks/bubbles/BubblesFragment.kt
@@ -1,9 +1,15 @@
 package team.lf.uitasks.bubbles
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import team.lf.uitasks.R
 
@@ -15,24 +21,64 @@ import team.lf.uitasks.R
  * Если пользователь поставит палец, а затем уберет его с оружности то окружность снова начнет двигаться.
  * */
 
-class BubblesFragment :Fragment(){
+const val IMAGE_VIEW_WIDTH_IN_DP = 100
+const val IMAGE_VIEW_HEIGHT_IN_DP = 100
+
+class BubblesFragment : Fragment() {
+
+    lateinit var displayMetrics: DisplayMetrics
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_bubbles, container, false)
-
-
-        return  view
-
+        displayMetrics = DisplayMetrics()
+        requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val root = inflater.inflate(R.layout.fragment_bubbles, container, false)
+        val rootWidth = (displayMetrics.widthPixels * displayMetrics.density + 0.5f)
+        val rootHeight = (displayMetrics.heightPixels * displayMetrics.density + 0.5f)
+        val ivWidth = (displayMetrics.density * IMAGE_VIEW_WIDTH_IN_DP + 0.5f).toInt()
+        val ivHeight = (displayMetrics.density * IMAGE_VIEW_HEIGHT_IN_DP + 0.5f).toInt()
+        (root as ConstraintLayout).addImageView(ivWidth, ivHeight, rootWidth, rootHeight)
+        return root
     }
-
-
 
     companion object {
         @JvmStatic
         fun newInstance(): BubblesFragment = BubblesFragment()
     }
+}
+
+private fun ConstraintLayout.addImageView(
+    ivWidth: Int,
+    ivHeight: Int,
+    rootWidth: Float,
+    rootHeight: Float
+) {
+
+    val imageView = ImageView(context).apply {
+        setImageDrawable(
+            ResourcesCompat.getDrawable(
+                resources,
+                R.drawable.ic_brightness_1_black_24dp,
+                null
+            )
+        )
+        layoutParams = ViewGroup.LayoutParams(ivWidth, ivHeight)
+    }
+    imageView.setAnimator(rootWidth, rootHeight, ivWidth, ivHeight)
+    addView(imageView)
+}
+
+private fun ImageView.setAnimator(
+    rootWidth: Float,
+    rootHeight: Float,
+    ivWidth: Int,
+    ivHeight: Int
+) {
+
+    Log.d("TAG", "$rootHeight $rootWidth $ivHeight $ivWidth")
+        val animator = ObjectAnimator.ofFloat(this, View.TRANSLATION_X, (rootWidth-ivWidth/2))
+        animator.start()
 }
