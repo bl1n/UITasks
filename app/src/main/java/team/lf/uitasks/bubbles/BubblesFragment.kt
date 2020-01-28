2b0cd563af20adedf6f3826dc86f09575f53f87e
sukha <sukhanov87@gmail.com>
2020-01-28 15:09:49 +0300

    bubbles

--- app/src/main/java/team/lf/uitasks/bubbles/BubblesFragment.kt
+++ app/src/main/java/team/lf/uitasks/bubbles/BubblesFragment.kt
@@ -1,18 +1,19 @@
 package team.lf.uitasks.bubbles
 
 import android.animation.Animator
-import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.annotation.SuppressLint
-import android.graphics.Point
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.animation.addPauseListener
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import team.lf.uitasks.R
@@ -26,97 +27,135 @@ import kotlin.random.Random
  * Если пользователь поставит палец, а затем уберет его с оружности то окружность снова начнет двигаться.
  * */
 
-const val IMAGE_VIEW_WIDTH_IN_DP = 100
-const val IMAGE_VIEW_HEIGHT_IN_DP = 100
+const val BUBBLE_WIDTH_IN_DP = 100
+const val BUBBLE_HEIGHT_IN_DP = 100
+const val NUMBER_OF_BUBBLES = 3
 
 class BubblesFragment : Fragment() {
 
-    lateinit var displayMetrics: DisplayMetrics
+    companion object {
+        @JvmStatic
+        fun newInstance(): BubblesFragment = BubblesFragment()
+    }
+    private var countOfTouched = 0
+
+    private lateinit var listOfX: List<Int>
+    private lateinit var listOfY: List<Int>
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        displayMetrics = DisplayMetrics()
-        requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
         val root = inflater.inflate(R.layout.fragment_bubbles, container, false)
-        val rootWidth = displayMetrics.widthPixels /displayMetrics.density
+
+        val displayMetrics = DisplayMetrics()
+        requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        val rootWidth = displayMetrics.widthPixels
         val rootHeight = displayMetrics.heightPixels
-        val ivWidth = (displayMetrics.density * IMAGE_VIEW_WIDTH_IN_DP + 0.5f).toInt()
-        val ivHeight = (displayMetrics.density * IMAGE_VIEW_HEIGHT_IN_DP + 0.5f).toInt()
-        (root as ConstraintLayout).addImageView(ivWidth, ivHeight, rootWidth.toInt(), rootHeight)
+        val ivWidth = (displayMetrics.density * BUBBLE_WIDTH_IN_DP + 0.5f).toInt()
+        val ivHeight = (displayMetrics.density * BUBBLE_HEIGHT_IN_DP + 0.5f).toInt()
+
+        listOfX = listOf(0, rootWidth / 2, rootWidth)
+        listOfY = listOf(0, rootHeight / 2, rootHeight)
+
+        for (i in 0 until NUMBER_OF_BUBBLES)
+            addImageView((root as ConstraintLayout), ivWidth, ivHeight)
         return root
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(): BubblesFragment = BubblesFragment()
+    private fun checkBubbles(count: Int) {
+        if (count == NUMBER_OF_BUBBLES)
+            Toast.makeText(
+                context,
+                "CAUGHT!",
+                Toast.LENGTH_SHORT
+            ).show() //todo add dialog with("Success!")
     }
-}
 
-private fun ConstraintLayout.addImageView(
-    ivWidth: Int,
-    ivHeight: Int,
-    rootWidth: Int,
-    rootHeight: Int
-) {
+    private fun addImageView(
+        constraintLayout: ConstraintLayout,
+        ivWidth: Int,
+        ivHeight: Int
+    ) {
 
-    val imageView = ImageView(context).apply {
-        setImageDrawable(
-            ResourcesCompat.getDrawable(
-                resources,
-                R.drawable.ic_brightness_1_black_24dp,
-                null
+        val imageView = ImageView(constraintLayout.context).apply {
+            setImageDrawable(
+                ResourcesCompat.getDrawable(
+                    resources,
+                    R.drawable.ic_brightness_1_black_24dp,
+                    null
+                )
             )
+            layoutParams = ViewGroup.LayoutParams(ivWidth, ivHeight)
+        }
+        imageView.setOnClickListener {
+            it.animation
+        }
+        setAnimator(imageView, listOfX, listOfY)
+        constraintLayout.addView(imageView)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setAnimator(
+        imageView: ImageView,
+        listOfX: List<Int>,
+        listOfY: List<Int>
+    ) {
+
+        val nextX = listOfX[Random.nextInt(listOfX.size)]
+        val nextY = listOfY[Random.nextInt(listOfY.size)]
+        Log.d("TAG", "$nextX $nextY ")
+        val pvhX = PropertyValuesHolder.ofFloat(
+            View.TRANSLATION_X,
+            nextX.toFloat()
         )
-        layoutParams = ViewGroup.LayoutParams(ivWidth, ivHeight)
+        val pvhY =
+            PropertyValuesHolder.ofFloat(
+                View.TRANSLATION_Y,
+                nextY.toFloat()
+            )
+        val animator = ObjectAnimator.ofPropertyValuesHolder(imageView, pvhX, pvhY)
+        animator.duration = 2000
+
+        animator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                setAnimator(imageView, listOfX, listOfY)
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+        })
+        animator.start()
+
+        imageView.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    animator.pause()
+                    countOfTouched++
+                    checkBubbles(countOfTouched)
+                }
+                MotionEvent.ACTION_UP -> {
+                    animator.resume()
+                    countOfTouched--
+                }
+            }
+            false
+        }
     }
-    imageView.setOnClickListener {
-        it.animation
-    }
-    imageView.setAnimator(rootWidth, rootHeight, ivWidth, ivHeight)
-    addView(imageView)
+
 }
 
-@SuppressLint("ClickableViewAccessibility")
-private fun ImageView.setAnimator(
-    rootWidth: Int,
-    rootHeight: Int,
-    ivWidth: Int,
-    ivHeight: Int
-) {
-    Log.d("TAG", "$rootWidth $rootHeight $ivHeight $ivWidth")
-    val pvhX = PropertyValuesHolder.ofFloat(View.TRANSLATION_X, 0f, (rootWidth-ivWidth).toFloat())
-    val pvhY =
-        PropertyValuesHolder.ofFloat(View.TRANSLATION_Y, 0f, (rootHeight - ivHeight).toFloat())
-    val animator = ObjectAnimator.ofPropertyValuesHolder(this, pvhX, pvhY)
-    animator.duration = 4000
-
-    animator.addListener(object : Animator.AnimatorListener {
-        override fun onAnimationRepeat(animation: Animator?) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun onAnimationEnd(animation: Animator?) {
-            this@setAnimator.setAnimator(rootWidth, rootHeight, ivWidth, ivHeight)
-        }
-
-        override fun onAnimationCancel(animation: Animator?) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun onAnimationStart(animation: Animator?) {
-        }
-    })
-    animator.start()
 
 
-    this.setOnTouchListener { v, event ->
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> animator.pause()
-            MotionEvent.ACTION_UP -> animator.resume()
-        }
-        true
-    }
-}
+
+
