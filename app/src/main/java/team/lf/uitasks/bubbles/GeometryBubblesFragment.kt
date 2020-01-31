b0dc97c7f495959196811d64e923940b5f83472e
sukha <sukhanov87@gmail.com>
2020-01-31 09:23:29 +0300

    they move!

--- app/src/main/java/team/lf/uitasks/bubbles/GeometryBubblesFragment.kt
+++ app/src/main/java/team/lf/uitasks/bubbles/GeometryBubblesFragment.kt
@@ -2,20 +2,27 @@ package team.lf.uitasks.bubbles
 
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
-import android.app.AlertDialog
 import android.os.Bundle
-import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
 import android.util.DisplayMetrics
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
+import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.marginLeft
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_bubbles.*
 import team.lf.uitasks.R
+import java.util.*
 import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.random.Random
 
 
@@ -26,9 +33,9 @@ class GeometryBubblesFragment : Fragment() {
 
         const val BUBBLE_WIDTH_IN_DP = 100
         const val BUBBLE_HEIGHT_IN_DP = 100
-
-
     }
+
+    private var numberOfBubbles = 0
 
     private var bottom: Float = 0f
     private var top: Float = 0f
@@ -36,12 +43,12 @@ class GeometryBubblesFragment : Fragment() {
     private var left: Float = 0f
     private var rootHeight: Int = 0
     private var rootWidth: Int = 0
-    private lateinit var timer: CountDownTimer
-    private var numberOfBubbles = 0
-    private lateinit var listOfAngles: MutableList<Float>
-
     private var ivWidth = 0
     private var ivHeight = 0
+    private var timerList = mutableListOf<Timer>()
+
+
+    private lateinit var listOfAngles: MutableList<Int>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,27 +57,102 @@ class GeometryBubblesFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_bubbles, container, false)
         numberOfBubbles = Random.nextInt(1, 7)
+        val time = numberOfBubbles * 2
 
-        initCorners()
+        initDimensions()
 
         populateListOfAngles()
+        addImageView(root as ViewGroup, ivWidth, ivHeight)
+        addImageView(root as ViewGroup, ivWidth, ivHeight)
 
-        addImView(root as ViewGroup)
-
-        startCounter()
         return root
+    }
+
+    private fun addImageView(
+        viewGroup: ViewGroup,
+        ivWidth: Int,
+        ivHeight: Int
+    ) {
+        val imageView = ImageView(viewGroup.context).apply {
+            setImageDrawable(
+                ResourcesCompat.getDrawable(
+                    resources,
+                    R.drawable.ic_brightness_1_black_24dp,
+                    null
+                )
+            )
+            layoutParams = ViewGroup.LayoutParams(ivWidth, ivHeight)
+        }
+
+        imageView.setOnClickListener {
+            it.animation
+        }
+        viewGroup.addView(imageView)
+        startTimer(imageView, getAngle())
+    }
+
+    private fun getAngle(): Int  = listOfAngles[Random.nextInt(listOfAngles.size)]
+
+    fun toRad(grad: Int) = grad * PI / 180
+
+    private fun startTimer(view: View, initAngle: Int) {
+        val timer = Timer()
+        timerList.add(timer)
+        var currentAngle = initAngle
+        var gipo = 50
+
+        (view.layoutParams as LinearLayout.LayoutParams).marginStart =Random.nextInt(0, right.toInt())
+        (view.layoutParams as LinearLayout.LayoutParams).topMargin = Random.nextInt(0, bottom.toInt())
+
+        timer.schedule(
+            object : TimerTask() {
+                override fun run() {
+                    if (view.left.toFloat() == left) {
+                        currentAngle =- currentAngle
+                    } else if (view.right.toFloat() == right) {
+                        currentAngle =- currentAngle
+
+                    } else if (view.top.toFloat() == top) {
+                        currentAngle =- currentAngle
+
+                    } else if (view.bottom.toFloat() == bottom) {
+                        currentAngle =- currentAngle
+
+                    }
+
+                    gipo += 50
+                    val nextX = gipo * cos(currentAngle.toFloat())
+                    val nextY = gipo * sin (currentAngle.toFloat())
+                    Log.d("TAG", "in timer $nextX, $nextY")
+                    Handler(Looper.getMainLooper()).post{
+                        ObjectAnimator.ofPropertyValuesHolder(
+                            view,
+                            PropertyValuesHolder.ofFloat( View.TRANSLATION_X, nextX),
+                            PropertyValuesHolder.ofFloat( View.TRANSLATION_Y, nextY)
+                        ).apply {
+                            interpolator = LinearInterpolator()
+                            duration = 100
+                        }
+                            .start()
+                    }
+
+                }
+            },
+            0,
+            100
+        )
+
     }
 
     private fun populateListOfAngles() {
         listOfAngles = mutableListOf()
         for (i in 0..360 step 45) {
-            val angle = i* PI/180
-            Log.d("TAG", "$i $angle")
-            listOfAngles.add(angle.toFloat())
+            Log.d("TAG", "$i")
+            listOfAngles.add(i)
         }
     }
 
-    private fun initCorners() {
+    private fun initDimensions() {
         val displayMetrics = DisplayMetrics()
         requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
         ivWidth = (displayMetrics.density * BUBBLE_WIDTH_IN_DP + 0.5f).toInt()
@@ -82,88 +164,16 @@ class GeometryBubblesFragment : Fragment() {
         right = (rootWidth - ivWidth).toFloat()
         top = 0f
         bottom = (rootHeight - ivHeight).toFloat()
+
+
     }
 
-    private fun addImView(root: ViewGroup) {
-        val imageView = ImageView(root.context).apply {
-            setImageDrawable(
-                ResourcesCompat.getDrawable(
-                    resources,
-                    R.drawable.ic_brightness_1_black_24dp,
-                    null
-                )
-            )
-            layoutParams = ViewGroup.LayoutParams(ivWidth, ivHeight)
-
-        }
-        addAnimator(imageView)
-        root.addView(imageView)
-    }
-
-    private fun addAnimator(imageView: View) {
-
-        var currentX = right / 2
-        var currentY = bottom / 2
-
-
-//        var nextXY: Pair<Float, Float> = getNextXY(currentX, currentY)
-
-        val pvhX = PropertyValuesHolder.ofFloat(
-            View.TRANSLATION_X,
-            currentX, left
-        )
-
-        val pvhY =
-            PropertyValuesHolder.ofFloat(
-                View.TRANSLATION_Y,
-                currentY, bottom
-
-            )
-        val animator = ObjectAnimator.ofPropertyValuesHolder(imageView, pvhX, pvhY)
-        animator.duration = Random.nextLong(500, 2000)
-        animator.start()
-    }
-
-//    private fun getNextXY(currentX: Float, currentY: Float): Pair<Float, Float> {
-//
-//    }
-
-    private fun startCounter() {
-        timer = object : CountDownTimer((numberOfBubbles * 2 * 1000).toLong(), 1000) {
-            override fun onFinish() {
-                startDialog("Время вышло!")
-            }
-
-            override fun onTick(millisUntilFinished: Long) {
-                counter.text = if (millisUntilFinished >= 1000) {
-                    millisUntilFinished.toInt().toString().subSequence(0, 1)
-                } else {
-                    "0"
-                }
-            }
-        }
-        timer.start()
-    }
-
-    private fun checkBubbles(count: Int) {
-        if (count == numberOfBubbles) {
-            timer.cancel()
-            startDialog("Success!")
+    override fun onPause() {
+        super.onPause()
+        timerList.forEach {
+            it.cancel()
         }
     }
 
-    fun startDialog(string: String) {
-        val builderDialog = AlertDialog.Builder(requireActivity())
-        builderDialog.setTitle(string)
-        builderDialog.setPositiveButton("Повторим!") { dialog, which ->
-            startCounter()
-            dialog.cancel()
-        }
-        builderDialog.setNegativeButton("Больше не хочу!") { dialog, which ->
-            requireActivity().onBackPressed()
-            dialog.cancel()
-        }
-        builderDialog.create().show()
-    }
 
 }
