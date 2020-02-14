3dd40a0ddc98125e875ad3585232ab276f34e2a5
sukha <sukhanov87@gmail.com>
2020-02-14 10:53:45 +0300

    gmail init

--- app/src/main/java/team/lf/uitasks/gmail/GmailFragment.kt
+++ app/src/main/java/team/lf/uitasks/gmail/GmailFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import team.lf.uitasks.R
 
 class GmailFragment:Fragment() {
@@ -19,6 +22,25 @@ class GmailFragment:Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return layoutInflater.inflate(R.layout.fragment_gmail, container,false)
+
+
+        val view = layoutInflater.inflate(R.layout.fragment_gmail, container,false)
+
+        val toolbar = view.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val bottomSheet  = view.findViewById<ViewGroup>(R.id.bottom_sheet)
+        val sheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        view.findViewById<Button>(R.id.bottom_sheet_button)
+            .setOnClickListener {
+                if(sheetBehavior.state == BottomSheetBehavior.STATE_HIDDEN){
+                    sheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+                } else {
+                    sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                }
+            }
+        return view
     }
 }
