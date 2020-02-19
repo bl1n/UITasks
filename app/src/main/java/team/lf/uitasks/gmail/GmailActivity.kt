af9f40822dadb209ed9ee708a403859a8a2efecf
sukha <sukhanov87@gmail.com>
2020-02-19 11:00:26 +0300

    bottom sheet behavior

--- app/src/main/java/team/lf/uitasks/gmail/GmailActivity.kt
+++ app/src/main/java/team/lf/uitasks/gmail/GmailActivity.kt
@@ -1,8 +1,11 @@
 package team.lf.uitasks.gmail
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import team.lf.uitasks.R
 
 class GmailActivity:AppCompatActivity() {
@@ -12,13 +15,38 @@ class GmailActivity:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_MaterialComponents_Light_NoActionBar)
         setContentView(R.layout.activity_gmail)
+
+        val shadowBox = findViewById<ViewGroup>(R.id.shadowBox)
+        val bottomSheet = findViewById<ViewGroup>(R.id.bottom_sheet)
+        val sheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
         findViewById<Button>(R.id.bottom_sheet_button)
             .setOnClickListener {
-                val fragment = ContactDetailsBottomSheetDialogFragment.newInstance()
-                fragment.apply {
-
-                }.show(supportFragmentManager, null)
+                if (sheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED){
+                    sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                    shadowBox.visibility = View.VISIBLE
+                }
             }
+
+        sheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback(){
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when(newState){
+                    BottomSheetBehavior.STATE_HIDDEN->{
+                        shadowBox.visibility = View.GONE
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING->{
+
+                    }
+
+                }
+            }
+
+        })
     }
 
 }
