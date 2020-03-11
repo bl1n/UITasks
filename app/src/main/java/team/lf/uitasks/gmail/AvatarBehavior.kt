52a0dc681e87aea8a422fa7741c0f73e9de70b37
Aleksandr Sukhanov <sa.android@e-legion.com>
2020-03-11 17:24:12 +0300

    homework

--- app/src/main/java/team/lf/uitasks/gmail/AvatarBehavior.kt
+++ app/src/main/java/team/lf/uitasks/gmail/AvatarBehavior.kt
@@ -2,13 +2,18 @@ package team.lf.uitasks.gmail
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.marginStart
 import com.google.android.material.appbar.AppBarLayout
+import kotlin.math.max
 
 class AvatarBehavior(context: Context, attributeSet: AttributeSet) :
     CoordinatorLayout.Behavior<ImageView>(context, attributeSet) {
+
+    private var startX: Float = 0f
 
     override fun layoutDependsOn(
         parent: CoordinatorLayout,
@@ -16,6 +21,19 @@ class AvatarBehavior(context: Context, attributeSet: AttributeSet) :
         dependency: View
     ): Boolean {
         return dependency is AppBarLayout
+    }
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: ImageView,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
+    ) {
+        startX = child.x
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
     }
 
     override fun onDependentViewChanged(
@@ -27,8 +45,23 @@ class AvatarBehavior(context: Context, attributeSet: AttributeSet) :
 
         val transformation = getTransformationFor(appBarLayout, child)
         transformImageView(transformation, child, appBarLayout)
+        val allRoute = child.x
+        moveImageView(transformation, child, appBarLayout, allRoute)
 
         return super.onDependentViewChanged(parent, child, dependency)
+    }
+
+    private fun moveImageView(
+        transformation: Float,
+        child: ImageView,
+        appBarLayout: AppBarLayout,
+        allRoute: Float
+    ) {
+        Log.d("TAG", "$startX")
+        if (transformation <= 0.5f) {
+            child.x -=1
+
+        }
     }
 
     private fun transformImageView(
@@ -36,14 +69,16 @@ class AvatarBehavior(context: Context, attributeSet: AttributeSet) :
         child: ImageView,
         appBarLayout: AppBarLayout
     ) {
-        val minHeight = appBarLayout.minimumHeight.toFloat()
-        child.scaleY = transformation
-        child.scaleX = maxOf(transformation, minHeight)
+        child.scaleY = max(transformation, MIN_SCALE)
+        child.scaleX = max(transformation, MIN_SCALE)
     }
 
     private fun getTransformationFor(appBarLayout: AppBarLayout, child: ImageView): Float {
         return (appBarLayout.totalScrollRange.toFloat() + appBarLayout.top.toFloat()) / appBarLayout.totalScrollRange.toFloat()
     }
 
+    companion object {
+        val MIN_SCALE = 0.5f
+    }
 
 }
